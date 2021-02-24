d9d46b7c9db2924ab1a313a609ee59a97b6992fb
André Cerqueira <acerqueira021@gmail.com>
2021-02-24 21:56:18 +0000

    Robor a ter atencao a distancias

--- src/man/Motorista.java
+++ src/man/Motorista.java
@@ -1,54 +1,70 @@
 package man;
+
 import java.awt.Color;
 import robocode.*;
 
- public class Motorista extends Robot {
-	 
-     public void run() {
+public class Motorista extends Robot {
+	
+	boolean lockedEnemy = false;
 
-         setColors(Color.green,Color.green,Color.white); // body / gun / radar
+	public void run() {
 
-         // Robot Main Loop
-         while (true) {
-             
-             ahead(100);
-             turnRight(45);
-             turnGunRight(45);
-             ahead(100);
-             turnRight(45);
-             turnGunRight(45);
-             
-         }
-     }
+		setColors(Color.black, Color.black, Color.white); // body / gun / radar
 
-     public void onScannedRobot(ScannedRobotEvent event) {
-         
-         double distance = event.getDistance();
-         double bearing = event.getBearing();
-         if (distance < 60) {
-             fire(3);
-             if(!(bearing > -90 && bearing < 90 )) {
-                 turnRight(180);
-             }
-         }else if(distance > 250 ) {
-             ahead(150);
-             if(!(bearing > -90 && bearing < 90 )) {
-                 turnRight(180);
-             }
-         }
-         else {
-             fire(1);
-         }
-         
-     }
-     //acertar tiro 
-     public void onHitRobot(HitRobotEvent event){
-         if(event.getBearing() > -90 && event.getBearing() <90 ) {
-             back(100);
-         }
-         else {
-             ahead(100);
-         }
-     }
+		// Robot Main Loop
+		while (true) {
+			move();
+		}
 
- }
+	}
+
+	public void move() {
+		ahead(200);
+		turnRight(45);
+		if (!lockedEnemy) {
+			turnGunRight(360);
+		}
+	}
+
+	// Quando encontra o adversario
+	public void onScannedRobot(ScannedRobotEvent event) {
+
+		double maxPower = Rules.MAX_BULLET_POWER;
+		double distance = event.getDistance();
+		double bearing = event.getBearing();
+
+		// Rodar para o adversario
+		if (bearing < 180) 
+			turnRight(bearing);
+		else 
+			turnLeft(bearing);
+
+		if (distance < 100) {
+			lockedEnemy=true;
+			fire(maxPower);
+		} else if(distance < 300){
+			fire(5);
+			ahead(distance/3);
+		}
+		else { 
+			lockedEnemy=false;
+			fire(1);
+		}
+		
+	}
+
+	public void onHittWall(HitWallEvent event) {
+		turnLeft(180);
+	}
+
+	/*
+	 * // Quando acerta o tiro 
+	 * public void onHitRobot(HitRobotEvent event){ }
+	 */
+
+	public void onHitByBulletEvent(HitByBulletEvent event) {
+			turnRight(90);
+			ahead(150);
+	}
+
+}
