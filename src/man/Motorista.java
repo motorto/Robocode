aca0e16207f88209d9e1e22b1fd341fb7320b56d
André Cerqueira <acerqueira021@gmail.com>
2021-02-24 18:24:55 +0000

    remove .classpath

--- src/man/Motorista.java
+++ src/man/Motorista.java
@@ -3,7 +3,11 @@ import java.awt.Color;
 import robocode.*;
 
  public class Motorista extends Robot {
+	 
+
 	      public void run() {
+	    	     setColors(Color.pink,Color.pink,Color.white); // body / gun / radar
+
 	          while (true) {
 	              ahead(100);
 	              turnGunRight(360);
