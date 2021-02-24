1d44382f1787afec9bb80c0a5e2b8cca3a18b8e7
Duarte <up201805437@fc.up.pt>
2021-02-24 18:04:49 +0000

    1o teste

--- src/man/Motorista.java
+++ src/man/Motorista.java
@@ -1,6 +1,5 @@
 package man;
 import java.awt.Color;
-
 import robocode.*;
 
  public class Motorista extends Robot {
@@ -10,14 +9,45 @@ import robocode.*;
     	 
     	 // Robot Main Loop
          while (true) {
+        	 /*
              ahead(100);
-             turnGunRight(360);
-             back(100);
-             turnGunRight(360);
+             turnRight(45);
+             turnGunRight(45);
+             ahead(100);
+             turnRight(45);
+             turnGunRight(45);
+             */
          }
      }
 
-     public void onScannedRobot(ScannedRobotEvent e) {
-         fire(1);
+     public void onScannedRobot(ScannedRobotEvent event) {
+    	 /*
+    	 double distance = event.getDistance();
+    	 double bearing = event.getBearing();
+    	 if (distance < 60) {
+             fire(3);
+             if(!(bearing > -90 && bearing < 90 )) {
+    			 turnRight(180);
+    		 }
+         }else if(distance > 250 ) {
+        	 ahead(150);
+        	 if(!(bearing > -90 && bearing < 90 )) {
+    			 turnRight(180);
+    		 }
+         }
+         else {
+             fire(1);
+         }
+    	 */
      }
+     //acertar tiro 
+     public void onHitRobot(HitRobotEvent event){
+    	 /*if(event.getBearing() > -90 && event.getBearing() <90 ) {
+    		 back(100);
+    	 }
+    	 else {
+    		 ahead(100);
+    	 }*/
+     }
+
  }
