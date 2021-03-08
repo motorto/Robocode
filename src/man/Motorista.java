b6994eb1c086c523e554093ff28cc6acc451b0a8
Duarte <up201805437@fc.up.pt>
2021-03-08 17:06:31 +0000

    motorista 1.4

--- src/man/Motorista.java
+++ src/man/Motorista.java
@@ -3,19 +3,20 @@ import java.awt.Color;
 import robocode.*;
 
 public class Motorista extends AdvancedRobot {
-	private int moveDirection = 1; // change dir
+	private int moveDirection = 1; // change direction
 	private int shots = 0; // Number of shots fired
-	private int enemyEnergy = 100;
+	private int enemyEnergy = 100; //inicial enemy energy
 
 	public void run() {
 		setColors(Color.pink,Color.pink,Color.white); // body / gun / radar
-		setBulletColor(Color.pink);
-		setAdjustGunForRobotTurn(true);
-		setAdjustRadarForGunTurn(true);
+		setBulletColor(Color.pink); 
+		setAdjustGunForRobotTurn(true);//gun independent from body
+		setAdjustRadarForGunTurn(true);//radar independent from gun
 
-		turnRadarRightRadians(Double.POSITIVE_INFINITY);
+		turnRadarRightRadians(Double.POSITIVE_INFINITY); //turns until scanned robot
+		
 		while (true) {
-			scan();
+			scan();//as there are only one enemy force scaned robot event 
 		}
 	}
 
@@ -23,14 +24,18 @@ public class Motorista extends AdvancedRobot {
 		int lastEnemyEnergy = enemyEnergy;
 		boolean lowEnergy = false;
 		
+		//get turn required for scan -> (enemy angle - current radar heading)
 		double radarTurn = getHeading() + event.getBearing()-getRadarHeading();
-
+				
 		setTurnRadarRight(normalizeBearing(radarTurn));
-
+		
+		//stay (parallel) with enemy
 		setTurnRight(normalizeBearing(event.getBearing() + 90 - (15 * moveDirection)));
+		//get turn required for gun -> (enemy angle - current gun heading)
 		setTurnGunRight(normalizeBearing(
 				(getHeading() + event.getBearing()) - getGunHeading()));
-		setAhead(200 * moveDirection ); // move in circles;
+		//move in circles
+		setAhead(200 * moveDirection ); 
 
 		if (getEnergy() < 1.0) 
 			lowEnergy = true;
@@ -40,20 +45,19 @@ public class Motorista extends AdvancedRobot {
 		if (!lowEnergy) { // If low energy , we dont shot we just move
 			if (event.getDistance() < 20)//bots touching
 				fire(Rules.MAX_BULLET_POWER);
-			else if(event.getDistance() < 120 ) {
+			else if(event.getDistance() < 100 ) {//bots close
 				fire(3);
 			}
-			else 
-				fire(0.5);
+			else //bots far away
+				fire(1);
 			
 			shots++;
 		}
 
-		// if enemyShoots we change dir with 50% chance
+		
 		enemyEnergy  = (int) event.getEnergy();
-
+		// if enemyShoots we change direction with 33.(3)% chance
 		if ((lastEnemyEnergy > enemyEnergy) && random(shots)) {
-			System.out.println("Entrei no random com o shot " + shots);
 			moveDirection *= -1;
 		}
 		AvoidWall();
@@ -63,7 +67,8 @@ public class Motorista extends AdvancedRobot {
 		if (x % 3 == 0) return true;
 		else return false;
 	}
-
+	
+	//change direction on hit wall
 	public void onHitWall(HitWallEvent event) {
 		moveDirection*=-1;
 	}
@@ -78,11 +83,13 @@ public class Motorista extends AdvancedRobot {
 		}
 		return angle;
 	}
-
+	
+	//find next enenmy
 	public void onRobotDeath(RobotDeathEvent event) {
 		turnRadarRightRadians(Double.POSITIVE_INFINITY);
 	}
-
+	
+	//dance
 	public void onWin(WinEvent event) {
 		while(true) {
 			turnRadarLeft(30);
@@ -91,17 +98,14 @@ public class Motorista extends AdvancedRobot {
 		}
 	}
 
-
-	// Em vez de um avoidWall , talvez um brake antes de bater na parede nao sei (reduzir a velocidade
-	//Ao minimo para tirar menos dano.
-	//  setMaxVelocity(Math.abs(getTurnRemaining()) < 40 ? preferredVelocity : 0.1);
+	//avoids hitting with max speed 
 	public void AvoidWall() {
 		boolean closeToWall = false;
 		final int distance = 40;
 		double Xpos = getX();
 		double Ypos = getY();
 		if (Xpos< distance 						|| Ypos<distance || 
-				Xpos>getBattleFieldWidth()-distance || Ypos>getBattleFieldHeight()-distance) {
+			Xpos>getBattleFieldWidth()-distance || Ypos>getBattleFieldHeight()-distance) {
 			if (closeToWall==false) {
 				setMaxVelocity(3);
 				closeToWall=true; 
@@ -113,7 +117,7 @@ public class Motorista extends AdvancedRobot {
 		}
 		else {
 			closeToWall=false;
-			setMaxVelocity(8);
+			setMaxVelocity(Rules.MAX_VELOCITY);
 		}
 	}
 }
